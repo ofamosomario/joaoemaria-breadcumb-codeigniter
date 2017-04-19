9d55575e2c38c690b1702448972b307cf7858d18
Mario Augusto <tkyakow@gmail.com>
2017-04-19 13:05:53 -0300

    Im alive.

--- joaoemaria_helper.php
+++ joaoemaria_helper.php
@@ -1,57 +1,32 @@
 <?php
+    function joaoemaria($ignore_segment = array(), $list = FALSE, $attrs = array()) {
+        $ignore_segment = (!is_array($ignore_segment)) ? array($ignore_segment) : $ignore_segment;
 
-function joaoemaria($ignore_segment = array(), $list = FALSE, $attrs = array()) {
-
-
-    $ignore_segment = (!is_array($ignore_segment)) ? array($ignore_segment) : $ignore_segment;
-
-    $ci = &get_instance();
-    
-    /*
-     * irá pegar todos os URI.
-     */
-    $crumb = $ci->uri->segment_array();
-    $result = $ci->uri->total_segments();
-    
-    
-    $url_crumb = array();
-    
-    /*
-     * ucfirst irá me retonar o primeiro char em maiusculo.
-     */
-    foreach ($crumb as $key => $c):
-        // in_array procura no array se tem algum elemento especifico.
-        if (!in_array($key, $ignore_segment)) {
-            /*
-             * se o resultado for menor que um, não irá criar um link, caso o
-             * contrário irá criar o link de todo o caminho corretamente.
-             */
-            if ($result <= 1) {
-                $url_crumb[] = anchor(base_url().$crumb[1], ucfirst($c));
-            } if ($result <= 2) {
-                $url_crumb[] = anchor(base_url().$crumb[1].'/'.$c, ucfirst(str_replace(array('_', '-'), ' ', $c))); 
-            } else {
-                $url_crumb[] = ucfirst(str_replace(array('_', '-'), ' ', $c)); 
+        $ci = &get_instance();
+        
+        $crumb = $ci->uri->segment_array();
+        $result = $ci->uri->total_segments();
+        
+        $url_crumb = array();
+        
+        foreach ($crumb as $key => $c):
+            if (!in_array($key, $ignore_segment)) {
+                if ($result <= 1) {
+                    $url_crumb[] = anchor(base_url().$crumb[1], ucfirst($c));
+                } if ($result <= 2) {
+                    $url_crumb[] = anchor(base_url().$crumb[1].'/'.$c, ucfirst(str_replace(array('_', '-'), ' ', $c))); 
+                } else {
+                    $url_crumb[] = ucfirst(str_replace(array('_', '-'), ' ', $c)); 
+                }
             }
-        }
 
-    endforeach;
+        endforeach;
 
-    // da um implode adicionando '>' entre as palavras.
-    $fim = implode(' > ', $url_crumb);
+        $fim = implode(' > ', $url_crumb);
 
-    if ($list)
-        $fim = ul($url_crumb, $attrs);
+        if ($list)
+            $fim = ul($url_crumb, $attrs);
 
-    if (!empty($fim))
-        return $fim;
-}
-
-    /*
-    echo joaoemaria(); // default out put
-    echo joaoemaria(1); // this will ignore first segment of url
-    echo joaoemaria(array(1,4)) // this will ignore first and 4th segment for crumb 
-    echo joaoemaria(1,TRUE); // this will create unorder html list
-    echo joaoemaria(1,TRUE,array('class'=>'test')); // can define attributes for ul list
-    echo joaoemaria(null,TRUE,array('class'=>'test')); // this will print all segment of url in crumb
-     */
+        if (!empty($fim))
+            return $fim;
+    }
